dfa6e2b065f43603fb0ca5c1191ecae448e6cf9d
Truc Kieu <kieudongoctruc@gmail.com>
2017-10-10 23:57:25 +0700

    set up swagger page where displaying all APIs documentation
    create users endpoint

---
+++ config/initializers/grape.rb
@@ -0,0 +1,4 @@
+GrapeSwaggerRails.options.url = "/api/v1/swagger_doc"
+GrapeSwaggerRails.options.app_name = "CodeNetwork"
+GrapeSwaggerRails.options.app_url = "https://codesocialnetwork.herokuapp.com"
+GrapeSwaggerRails.options.app_url = "http://localhost:3000" if ENV['RAILS_ENV'] == "development"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  mount API::V1::Base, at: '/'
+  mount GrapeSwaggerRails::Engine => '/documentation'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
